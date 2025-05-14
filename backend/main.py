405922e94f86e5d00aac996833762c3001c3dc11
Prince Adewale Adewumi <82568973+Oracle69digitalmarketing@users.noreply.github.com>
2025-05-14 20:04:07 +0100

    Create main.py

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from auth.routes import auth_router
+from api.routes import user_router
+
+app = FastAPI()
+
+origins = ["*"]  # You can restrict this later
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth_router, prefix="/api")
+app.include_router(user_router, prefix="/api")
