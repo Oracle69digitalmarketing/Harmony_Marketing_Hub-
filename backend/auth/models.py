e748949f687a251eb274915519ee6ab916c2aed9
Prince Adewale Adewumi <82568973+Oracle69digitalmarketing@users.noreply.github.com>
2025-06-15 21:05:40 +0100

    Update models.py

--- backend/auth/models.py
+++ backend/auth/models.py
@@ -9,7 +9,16 @@ class UserLogin(BaseModel):
     email: str
     password: str
 
-from sqlalchemy import Boolean, Column, String
+from sqlalchemy import Column, String, Boolean
+from db.database import Base
 
-verified = Column(Boolean, default=False)
-verification_token = Column(String, nullable=True, unique=True)
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(String, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    business_name = Column(String, nullable=True)
+
+    verified = Column(Boolean, default=False)
+    verification_token = Column(String, unique=True, nullable=True)
